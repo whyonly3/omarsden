33854c089805ab2d419b66d5da3fa14894866423
Esteban lau koo <Esteban lau koo>
2017-03-20 12:53:50 -0400

    input value and database

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,40 +1,68 @@
-function twitch() {
-    var twitchUrl= "https://api.twitch.tv/kraken/streams/?game=overwatch"
-    var query = $(this).attr('data-person')
-    $.ajax({
-        url: twitchUrl,
-        method: "GET",
-        headers: {"Client-ID": "ncvflt70y19rx0ayfvplqquyzqpilx"}
-    })
-}
-function giphy() {
-    var giphyUrl= "https://giphy.p.mashape.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=funny+cat"
-    var query = $(this).attr('data-person') // built text input apply to end of q=
-    $.ajax({
-        url: giphyUrl,
-        method: "GET",
-        headers: {"X-Mashape-Key": "rVAKBl1pWxmshN2KI8WzLgDtQvXSp1CLTDhjsnjZT2bL5VZLaQ",
-                  "Accept": "application/json"  },
-    })
-        .done(function (response) {
-            var results = response
-            console.log(results)
+
+$(document).ready(function () {
+    var config = {
+        apiKey: "AIzaSyBMxgWIvoUenp7EeSVDTWZ_moYKSA0LZS8",
+        authDomain: "omars-den.firebaseapp.com",
+        databaseURL: "https://omars-den.firebaseio.com",
+        storageBucket: "omars-den.appspot.com",
+        messagingSenderId: "283220849230"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database()
+    $('#search').on('click', function (e) {
+        var query = $('#query-input').val().trim()
+        var igdbUrl = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,url,summary,cover&search=" + query
+        var igdbAjax = $.ajax({
+            url: igdbUrl,
+            method: "GET",
+            headers: {
+                "X-Mashape-Key": "rVAKBl1pWxmshN2KI8WzLgDtQvXSp1CLTDhjsnjZT2bL5VZLaQ",
+                "Accept": "application/json"
+            },
         })
-}
-function igdb() {
-    var igdbUrl = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,url&search=overwatch"
-    var query  = $(this).attr('data-person') // build the text input apply to end of search =
-    $.ajax({
-        url: igdbUrl,
-        method: "GET",
-        headers: {"X-Mashape-Key": "rVAKBl1pWxmshN2KI8WzLgDtQvXSp1CLTDhjsnjZT2bL5VZLaQ",
-                    "Accept": "application/json"},
-    })
-        .done(function (response) {
-            var results = response
-            console.log(results)
+        alert("this is working!")
+        database.ref('main-request').set({
+            request : query
         })
-}
-igdb()
-giphy()
-twitch()
+            .done( function (response) {
+                console.log(response)
+            })
+
+    })
+
+    database.ref('main-request').on('child_added', function (a, b) {
+        console.log('child_added', a.val());
+    var search = a.val()
+        console.log(search)
+    var twitchUrl = "https://api.twitch.tv/kraken/streams/?game=" + search + "&limit=1"
+    var giphyUrl = "https://giphy.p.mashape.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=6&q=" + search
+    var igdbUrl = "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,url&search=" + search
+    var twitchAjax= $.ajax({
+            url: twitchUrl,
+            method: "GET",
+            headers: {"Client-ID": "ncvflt70y19rx0ayfvplqquyzqpilx"}
+        }),
+        giphyAjax = $.ajax({
+            url: giphyUrl,
+            method: "GET",
+            headers: {
+                "X-Mashape-Key": "rVAKBl1pWxmshN2KI8WzLgDtQvXSp1CLTDhjsnjZT2bL5VZLaQ",
+                "Accept": "application/json"
+            },
+        }),
+        igdbAjax = $.ajax({
+            url: igdbUrl,
+            method: "GET",
+            headers: {
+                "X-Mashape-Key": "rVAKBl1pWxmshN2KI8WzLgDtQvXSp1CLTDhjsnjZT2bL5VZLaQ",
+                "Accept": "application/json"
+            },
+        })
+    $.when(twitchAjax,giphyAjax,igdbAjax).done(function (r1,r2,r3) {
+        console.log(r1[0])
+        console.log(r2[0])
+        console.log(r3[0])
+    })
+})
+
+})
